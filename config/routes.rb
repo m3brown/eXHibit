2d9234b1b9491cd348c0ebf9accc6b0494dbbb32
uar-dejan-susic <dejan.susic@upandrunningsoftware.com>
2016-03-17 10:15:11 -0400

    Added devise and few other bits and peaces.  - taostr_rails  - simple_form  - slim_template / will not be used at the end but it is in there
     Created user model and moved few things around.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
   root 'dashboard#index'
+
+  get 'about' => 'dashboard#about', as: :about
+  get 'search_streams' => 'dashboard#search', as: :search_stream_path
+
+
+  devise_for :users
+  mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe User, type: :model do
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_length_of(:name) }
+
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+end
